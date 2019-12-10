a1fce7bfa6da0efb5c9f3d208d56c4edf53c9316
michal.nowak <michal.nowak@hycom.pl>
2019-12-10 13:13:22 +0100

    Added complete pics app

--- section-7/pics/src/components/ImageList.js
+++ section-7/pics/src/components/ImageList.js
@@ -1,13 +1,14 @@
+import './ImagesList.scss';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-
-    const images = props.images.map(({ description, id, urls }) => {
-        return <img key={id} src={urls.small} alt={description} />
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image}/>
     })
 
     return (
-        <div>
+        <div className='images-list'>
             {images}
         </div>
     )
